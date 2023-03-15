764fe1af041fcd16cb29b89affdb7e40c760bebe
Wagyourtail <wagyourtail@wagyourtail.xyz>
2023-03-15 13:21:06 -0700

    dont remap back - breaks fg3

--- src/minecraft/kotlin/xyz/wagyourtail/unimined/minecraft/MinecraftProviderImpl.kt
+++ src/minecraft/kotlin/xyz/wagyourtail/unimined/minecraft/MinecraftProviderImpl.kt
@@ -269,10 +269,10 @@ abstract class MinecraftProviderImpl(
                     "jar",
                     minecraft.getMinecraft(EnvType.SERVER)
                 )
-                mcRemapper.provide(mcPatcher.merge(
+                mcPatcher.merge(
                     mcRemapper.provide(client, mergeNamespace, mergeNamespace),
                     mcRemapper.provide(server, mergeNamespace, mergeNamespace)
-                ), MappingNamespace.OFFICIAL, MappingNamespace.OFFICIAL)
+                )
             } else {
                 MinecraftJar(
                     minecraft.mcVersionFolder(minecraft.version),
