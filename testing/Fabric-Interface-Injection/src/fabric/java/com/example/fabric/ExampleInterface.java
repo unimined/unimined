19bfd2253c7aeb731291d6227afa9732e2b9dd86
Cat Core <arthur.koalas@free.fr>
2023-11-18 00:03:52 +0100

    Add a test for Interface Injection

---
+++ testing/Fabric-Interface-Injection/src/fabric/java/com/example/fabric/ExampleInterface.java
@@ -0,0 +1,5 @@
+package com.example.fabric;
+
+public interface ExampleInterface {
+    String test();
+}
---
+++ testing/Fabric-Interface-Injection/src/main/java/com/example/mixin/ExampleMixin.java
@@ -0,0 +1,17 @@
+package com.example.mixin;
+
+import com.example.ExampleMod;
+import net.minecraft.client.gui.screens.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+
+    @Inject(method = "init()V", at = @At("HEAD"))
+    private void init(CallbackInfo info) {
+        ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+    }
+}
