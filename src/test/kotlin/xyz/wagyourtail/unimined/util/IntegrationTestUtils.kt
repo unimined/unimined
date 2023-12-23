b35b101a477ebbf46386e0e15ab8be873250c188
Cat Core <arthur.koalas@free.fr>
2023-12-23 19:08:16 +0100

    Don't run integration tests against gradle 7

--- src/test/kotlin/xyz/wagyourtail/unimined/util/IntegrationTestUtils.kt
+++ src/test/kotlin/xyz/wagyourtail/unimined/util/IntegrationTestUtils.kt
@@ -30,7 +30,7 @@ fun openZipFileSystem(project: String, path: String): FileSystem? {
 class IntegrationTestUtils {
     companion object {
         val GRADLE_VERSION = arrayOf(
-            "7.6.3",
+//            "7.6.3",
             "8.1.1",
             GRADLE_CURRENT,
             "8.5"
