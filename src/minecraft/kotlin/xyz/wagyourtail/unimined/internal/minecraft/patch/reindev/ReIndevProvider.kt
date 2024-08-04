8b2b570d24e36d8a40dfb4934898982107173294
Wagyourtail <wagyourtail@wagyourtail.xyz>
2024-08-04 07:10:57 -0500

    use new replaceLibraryVersion

--- src/minecraft/kotlin/xyz/wagyourtail/unimined/internal/minecraft/patch/reindev/ReIndevProvider.kt
+++ src/minecraft/kotlin/xyz/wagyourtail/unimined/internal/minecraft/patch/reindev/ReIndevProvider.kt
@@ -18,21 +18,10 @@ class ReIndevProvider(project: Project, sourceSet: SourceSet) : MinecraftProvide
     init {
         // Required for the following [2.9.4+legacyfabric.8,) dependency
         project.unimined.legacyFabricMaven()
-        minecraftLibraries.resolutionStrategy.eachDependency {
-            val group = it.requested.group
-            val name = it.requested.name
-            val module = "$group:$name"
-            if (module == "org.ow2.asm:asm-all") {
-                // Upgrades the class compatibility level to Java 8 to match ReIndev
-                it.useVersion("5.2")
-            } else if (group.startsWith("org.lwjgl")) {
-                // Fixes several bugs, including failure to launch on Linux
-                it.useVersion("[2.9.4+legacyfabric.8,)")
-            } else if (module == "net.java.jinput:jinput") {
-                // Upgrading the JInput version to the latest fixes minor bugs like incompatible version notices
-                it.useVersion("[${it.requested.version},2.0.9]")
-            }
-        }
+
+        replaceLibraryVersion("org.ow2.asm", "asm-all", version = "5.2")
+        replaceLibraryVersion("org.lwjgl", version = "2.9.4+legacyfabric.8")
+        replaceLibraryVersion("net.java.jinput", "jinput", version = "2.0.9")
 
         mappings.devNamespace = mappings.OFFICIAL
         mappings.devFallbackNamespace = mappings.OFFICIAL
