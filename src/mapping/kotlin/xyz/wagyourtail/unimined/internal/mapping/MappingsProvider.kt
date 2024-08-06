1c86f596bd8bf51028450cf5292540e18ed822a0
Wagyourtail <wagyourtail@wagyourtail.xyz>
2024-08-06 14:08:23 -0500

    oops

--- src/mapping/kotlin/xyz/wagyourtail/unimined/internal/mapping/MappingsProvider.kt
+++ src/mapping/kotlin/xyz/wagyourtail/unimined/internal/mapping/MappingsProvider.kt
@@ -473,9 +473,6 @@ class MappingsProvider(project: Project, minecraft: MinecraftConfig, subKey: Str
             requires("babricIntermediary")
             provides("nostalgia" to true)
             mapNamespace("intermediary" to "babricIntermediary", "named" to "nostalgia")
-            afterLoad.add {
-                renest(requires.name, *provides.map { it.first.name }.toTypedArray())
-            }
             action()
         }
         addDependency(key, entry)
